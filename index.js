2825631101d496156d4c7b6e05b2fa93e2d6df39
turpp <white.tripp@icloud.com>
2021-01-18 21:25:09 -0600

    commit tree 304fe7fac727340d58578875abe3a10894a0e3ab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(wrap="*"){
+    return function(adjective="special"){
+        return `You are ${wrap}${adjective}${wrap}!`
+    }
+}
+
+const Calculator = {
+    add: function(a, b){
+        return a+b
+    },
+    subtract: function(a, b){
+       return a-b
+    },
+    multiply: function(a,b){
+        return a*b
+    },
+    divide: function(a,b){
+        return a/b
+    }
+}
+
+function actionApplyer(int, array){
+    let base = int
+    if(array.length == 0){
+        return int
+    }else{
+        array.forEach(function(fn){
+
+           base = (fn(base))
+        })
+        return base
+    }
+}
